d68c6e8301c223ce55549401f3efbbcc10b9a31d
cas <119894136+casmdz@users.noreply.github.com>
2022-12-28 18:02:57 -0600

    wednesday

---
+++ beforechanges.py
@@ -0,0 +1,75 @@
+# You will need a few attributes as well:
+#     tickets -> list
+#     parkingSpaces -> list
+#     currentTicket -> dictionary
+
+        # self.tickets = []
+        # self.parkingSpaces = [] 
+        # self.currentTicket = {}
+    
+
+
+class parkingGarage():
+
+    tickets= 1
+    parkingSpaces = 10
+    #paid=true/false
+    currentTicket = {} 
+#currentticket is constantly getting manipulated as people come in 
+
+    def parkingSpaces(self):
+        return self.parkingSpaces
+
+#my garage = mine in parkinggarage, and is the self
+    def takeTicket(self):
+        if self.parkingSpaces == 0:
+            print("Sorry we are full")
+
+        else:
+            print(f'We have {self.parkingSpaces} spaces available')
+            print(f'Your ticket is ticket # {self.tickets}, please keep it safe in order to pay and leave the garage.')
+            
+            self.currentTicket[self.ticket] = {'paid': False}
+            self.parkingSpaces -= 1
+            self.tickets += 1
+
+            # user comes in
+            # takes ticket
+            # ticket gets a number and variable of paid 
+            #     spaces go down 1 
+            #return self.tickets (how many are avail)
+
+            #showCapacity()
+
+        if parkingGarage.tickets == 10:
+            print ('sorry no more tickets')
+
+        ticket_number= len(self.tickets) + 1 #from garage list... creates a ticket number by adding 1 to the list (which starts off as 0 []), and will keep adding one (so ticket #2, and #3 and so on...) so when we Take Ticket, we can see #1/2/3... paid or not
+        myticket = {
+            "ticket number": ticket_number, 
+            "paid": False}
+        self.currentTicket = myticket 
+        parkingGarage.parkingSpaces -= 1
+
+        # parkingGarage.tickets += 1
+        return self.tickets 
+        
+
+
+    def payForParking(self):
+        payment = ()
+        if payment:
+            self.currentTicket = True:
+            print('thanks for paying, you have 15 minutes to leave')
+    
+    #turn ticket (which is currently a string) you need to turn it into an integer 
+    # self.currentTicket[f'ticket number'][paid] = True
+    #  
+    def leaveGarage(self):
+        if payment:
+            self.currentTicket = True
+        print("Thank you have a nice a nice")
+
+
+
+print('Have a nice day!')
--- parkingGarage.py
+++ parkingGarage.py
@@ -1,66 +1,42 @@
-# You will need a few attributes as well:
-#     tickets -> list
-#     parkingSpaces -> list
-#     currentTicket -> dictionary
-
-        # self.tickets = []
-        # self.parkingSpaces = [] 
-        # self.currentTicket = {}
-    
-
-
 class parkingGarage():
 
-    tickets= 10
-    # parkingSpaces = 10
-    currentTicket = {} #paid=true/false
+    tickets= 1
+    parkingSpaces = 10
+    currentTicket = {} 
 
-    def __init__(self, tickets, parkingSpaces, currentickets):
-        
-        self.tickets = []
-        self.parkingSpaces = 10
-        self.currentTicket = {}
 
     def parkingSpaces(self):
         return self.parkingSpaces
 
 
     def takeTicket(self):
-            # user comes in
-            # takes ticket
-            # ticket gets a number and variable of paid 
-            #     spaces go down 1 
-            #return self.tickets (how many are avail)
+        if self.parkingSpaces == 0:
+            print("Sorry we are full")
 
-            #showCapacity()
-
-        if parkingGarage.tickets == 10:
-            print ('sorry no more tickets')
-
-        ticket_number= len(self.tickets) + 1 #from garage list... creates a ticket number by adding 1 to the list (which starts off as 0 []), and will keep adding one (so ticket #2, and #3 and so on...) so when we Take Ticket, we can see #1/2/3... paid or not
-        myticket = {
-            "ticket number": ticket_number, 
-            "paid": False}
-        self.currentTicket = myticket 
-        parkingGarage.parkingSpaces -= 1
-
-        # parkingGarage.tickets += 1
-        return self.tickets 
-        
+        else:
+            print(f'We have {self.parkingSpaces} spaces available')
+            print(f'Your ticket is ticket # {self.tickets}, please keep it safe in order to pay and leave the garage.')
+            
+            self.currentTicket[self.ticket] = {'paid': False}
+            self.parkingSpaces -= 1
+            self.tickets += 1
 
 
     def payForParking(self):
-        payment = ()
+        payment = input("Enter payment amount:  ")
         if payment:
-            self.currentTicket = True:
+            self.currentTicket[self.ticket]['paid'] = True 
             print('thanks for paying, you have 15 minutes to leave')
-    
+ 
+
 
     def leaveGarage(self):
-        if payment:
-            self.currentTicket = True
-        print("Thank you have a nice a nice")
-
+        if not self.current_ticket["paid"]:
+            payment = input("Enter payment amount: ")
+            if payment:
+                self.current_ticket["paid"] = True
+                print('thanks for paying, you have 15 minutes to leave')
+        
 
 
 print('Have a nice day!')
